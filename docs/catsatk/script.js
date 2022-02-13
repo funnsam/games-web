04ae1e112b0c1e986410b51b730ea96b167c32b3
funnsam <61924648+funnsam@users.noreply.github.com>
2022-02-13 18:33:16 +0800

    Add files via upload

---
+++ docs/catsatk/script.js
@@ -0,0 +1,12 @@
+function Buy() {
+    window.open("https://funnsam.itch.io/cats-attack", "https://funnsam.itch.io/cats-attack", "width=500, height=500");
+}
+function Help() {
+    window.open("https://www.kickstarter.com", "https://www.kickstarter.com");
+}
+function Free() {
+    window.open("https://www.kickstarter.com", "https://www.kickstarter.com");
+}
+function Home() {
+    window.open("../", "_self")
+}
